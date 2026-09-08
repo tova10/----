ff82a759bb74b553217b8bc1dd7aeee90bf3bc3a
Tova Revach <tova5343@gmail.com>
2026-09-09 00:04:20 +0300

    עדכון קובץ תיעוד
    עדכון של הקובץ TRANSACTIONSTORE.
    הקובץ הוא תיעוד למתכנת איך אמורה להיות צורה של נתונים שנחלצים מהאקסל.
    זהו קובץ תיעוד ועזרה בלבד טלא אחראי על שום ריצת קוד.

--- src/core/transavtionStore.js
+++ src/core/transavtionStore.js
@@ -1,2 +1,13 @@
 // מחזיק במקום אחד את כל ה-Transactions מכל קבצי האקסל שנטענו.
 // מאפשר להוסיף נתונים מכמה קבצים ולשלוף את כולם יחד לצורך סינון.
+
+
+/**
+ * @typedef {Object} Transaction
+ * @property {string} date
+ * @property {number} amount
+ * @property {string} sender
+ * @property {string} method
+ */
+ 
+
