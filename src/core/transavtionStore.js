4c47025b253e9bbeaea1223f9931fc00c18caff2
Tova Revach <tova5343@gmail.com>
2026-09-09 17:14:47 +0300

    כתיבת transaction.js
    עדכון הקובץ transaction .
    כתיבת הנתונים שיהיו לכל צק בתוכנית.

--- src/core/transavtionStore.js
+++ src/core/transavtionStore.js
@@ -2,12 +2,6 @@
 // מאפשר להוסיף נתונים מכמה קבצים ולשלוף את כולם יחד לצורך סינון.
 
 
-/**
- * @typedef {Object} Transaction
- * @property {string} date
- * @property {number} amount
- * @property {string} sender
- * @property {string} method
- */
+
  
 
--- src/types/transaction.js
+++ src/types/transaction.js
@@ -1,3 +1,16 @@
 // קובץ תיעוד בלבד - מגדיר איך נראית "תנועה" אחידה במערכת (Transaction)
 // ומה המבנה של תוצאת שאילתה (Result Object).
 // אין כאן קוד רץ - רק הגדרות מבנה לשימוש כל שאר הקבצים.
+
+
+/**
+ * @typedef {Object} Transaction
+ * 
+ * @property {string} date  
+ * @property {number} amount
+ * @property {string} sender
+ * @property {string} method - שיטת התשלום (כרטיס אשראי, העברה בנקאית, מזומן...)
+ * @property {"income"|"expense"} type - סוג התנועה (הכנסה או הוצאה)
+ * @property {Array<string>} emptyDetails - רשימת שדות ריקים (למשל אם אין מספר חשבון או תיאור)
+ * @property {string} fileName - שם הקובץ שממנו נלקחה התנועה
+ */
